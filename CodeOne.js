7eaad456cbce686592fca5889c66514931258c9a
AGABA Patrick <115509400+Cedric-BigG255@users.noreply.github.com>
2025-01-01 19:12:07 +0200

    Update CodeOne.js

--- CodeOne.js
+++ CodeOne.js
@@ -24,6 +24,22 @@ function isPalindrome(str) {
 
 // Test cases
 console.log(isPalindrome("racecar")); // true
+
+// The check of the greatest 
+
+const findMax = (num1, num2, num3) => {
+    if (num1 === num2 && num2 === num3) {
+        return "All numbers are the same.";
+    } else {
+        return Math.max(num1, num2, num3);
+    }
+};
+
+// Test the function
+console.log(findMax(10, 20, 30)); // Output: 30
+console.log(findMax(25, 25, 25)); // Output: All numbers are the same.
+console.log(findMax(50, 40, 60)); // Output: 60
+
 console.log(isPalindrome("hello")); // false
 console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
 console.log(isPalindrome("No lemon, no melon")); // true
